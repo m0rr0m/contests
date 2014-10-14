5957007e1837f2b6df412734309e603602699aa2
Maxim Komar <mk@mtw.ru>
2014-10-14 15:09:47 +0400

    more correct sort

--- darkus/october2014/darkus/n.pl
+++ darkus/october2014/darkus/n.pl
@@ -3,8 +3,8 @@ use strict;
 require '../common.pl';
 
 # [[[0,0,0,0],[0,0,0,0],[0,0,1,0],[0,1,1,1]], ...]
-my @res = sort uniq map { 
+my @res = sort { $a cmp $b } uniq (map { 
     rotate(join "\n", map { s/[\[,\]]//g; tr/01/.X/; $_ } split /\],\[/)
-} <> =~ /\[\[.+?\]\]/g;
+} <> =~ /\[\[.+?\]\]/g);
 
 print join "\n\n", @res;
