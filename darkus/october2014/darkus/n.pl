f448b1985c575c0c487f597e8d5aad56558764fb
Maxim Komar <mk@mtw.ru>
2014-10-14 11:12:45 +0400

    results added (n = 4)

--- darkus/october2014/darkus/n.pl
+++ darkus/october2014/darkus/n.pl
@@ -3,8 +3,8 @@ use strict;
 require '../common.pl';
 
 # [[[0,0,0,0],[0,0,0,0],[0,0,1,0],[0,1,1,1]], ...]
-my @res = sort map { 
-    join "\n", map { s/[\[,\]]//g; tr/01/.X/; $_ } split /\],\[/
+my @res = sort uniq map { 
+    rotate(join "\n", map { s/[\[,\]]//g; tr/01/.X/; $_ } split /\],\[/)
 } <> =~ /\[\[.+?\]\]/g;
 
 print join "\n\n", @res;
