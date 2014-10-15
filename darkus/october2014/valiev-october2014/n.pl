0fd2844730dbd8eb08416242e9420d14dc520ad8
Maxim Komar <mk@mtw.ru>
2014-10-15 13:41:12 +0400

    normalizer added

---
+++ darkus/october2014/valiev-october2014/n.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl -w
+use strict;
+require '../common.pl';
+
+# X _ _ _ 
+# _ _ _ _ 
+# X _ X _ 
+# _ X _ _ 
+# 
+
+undef $/;
+my @res = sort { $a cmp $b } uniq (map { s/^\n//; s/ //g; tr/_/./; $_ } split /\n\n/, <>);
+print join "\n\n", @res;
