cd0189cd7dd4a68a1f60540f40d11908a8f2771b
Maxim Komar <mk@mtw.ru>
2014-10-15 13:43:19 +0400

    more correct normalizer

--- darkus/october2014/valiev-october2014/n.pl
+++ darkus/october2014/valiev-october2014/n.pl
@@ -2,12 +2,12 @@
 use strict;
 require '../common.pl';
 
+# 
 # X _ _ _ 
 # _ _ _ _ 
 # X _ X _ 
 # _ X _ _ 
-# 
 
 undef $/;
-my @res = sort { $a cmp $b } uniq (map { s/^\n//; s/ //g; tr/_/./; $_ } split /\n\n/, <>);
+my @res = sort { $a cmp $b } uniq (map { s/^\n//; s/ //g; tr/_/./; rotate($_) } split /\n\n/, <>);
 print join "\n\n", @res;
