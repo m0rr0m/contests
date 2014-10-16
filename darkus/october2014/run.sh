01a7a05c9df7f2f8552b5692c987a9ce168267c7
Maxim Komar <mk@mtw.ru>
2014-10-16 07:43:17 +0400

    run: n = 6

--- darkus/october2014/run.sh
+++ darkus/october2014/run.sh
@@ -9,3 +9,13 @@ for i in `find . -maxdepth 1 -type d | grep -v '^.$'`; do
     make run
     cd ..
 done
+
+export N=6
+for i in `find . -maxdepth 1 -type d | grep -v '^.$' | grep -v darkus`; do
+    echo $i
+    cd $i
+    make
+    make check
+    make run
+    cd ..
+done
