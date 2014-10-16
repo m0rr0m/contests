88a5cf8e87a18e80f045aaf90c570c026f2d2668
Maxim Komar <mk@mtw.ru>
2014-10-16 11:06:33 +0400

    aggregate results added

--- darkus/october2014/aggregate_results.pl
+++ darkus/october2014/aggregate_results.pl
@@ -7,6 +7,35 @@ opendir DIR, $root;
 my @dir = grep { /^[^\.]/ } grep { -d "$root/$_" } readdir(DIR);
 closedir DIR;
 
-my @n = (4);
+# 0:00.66elapsed
+my $pat = qr/(\d+):(\d+\.\d+)elapsed/;
+my @n = (4, 6);
 
+my %h;
+foreach my $dir (@dir){
+    foreach my $n (@n){
+        $h{$dir}{$n} = extract_time($dir, $n);
+    }
+}
 
+my @table = (['N', @n]);
+push(@table, ['-', map { '--' } @n]);
+foreach my $dir (@dir){
+    my @res = map { $h{$dir}{$_} } @n;
+    push(@table, [$dir, @res]);
+}
+
+print (join "\n", map { '|' . (join '|', @$_) . '|' } @table);
+
+sub extract_time {
+    my ($user, $n) = @_;
+    local $/ = undef;
+    open(FILE, '<', "$user/time$n") or return '-';
+    my $str = <FILE>;
+    close FILE;
+
+    return unless  $str =~ $pat;
+    my ($m, $s) = ($1, $2);
+
+    return $m*60 + $s;
+}
